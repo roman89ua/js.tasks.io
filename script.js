6da1f5af89a21ebfeeffd008c0b7475aea82a43f
roman89ua <roman89ua@gmail.com>
2020-05-31 16:37:14 +0300

    done with the loking

--- script.js
+++ script.js
@@ -1448,6 +1448,36 @@ function work(a, b) {
 //   console.log(a)
 // }
 
+function throttle(func, ms) {
+
+	let isThrottled = false,
+		savedArgs,
+		savedThis;
+
+	function wrapper() {
+
+		if (isThrottled) { // (2)
+			savedArgs = arguments;
+			savedThis = this;
+			return;
+		}
+
+		func.apply(this, arguments); // (1)
+
+		isThrottled = true;
+
+		setTimeout(function () {
+			isThrottled = false; // (3)
+			if (savedArgs) {
+				wrapper.apply(savedThis, savedArgs);
+				savedArgs = savedThis = null;
+			}
+		}, ms);
+	}
+
+	return wrapper;
+}
+
 // // f1000 передаёт вызовы f максимум раз в 1000 мс
 // let f1000 = throttle(f, 1000);
 
