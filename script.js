f334f595d7cc540cef6d9740b56709d38c4863a4
roman89ua <roman89ua@gmail.com>
2020-06-13 21:38:25 +0300

    promise first task done

--- script.js
+++ script.js
@@ -1929,4 +1929,34 @@ function work(a, b) {
 
 // !!! Промисы, async/await
 
+function showCircle(cx, cy, radius) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+    }, 100);
+    callback(div);
+}
+
+function callback(domELement) {
+    let p = document.createElement('p');
+    let text = document.createTextNode('Hello, world!');
+    p.appendChild(text);
+    domELement.appendChild(p);
+
+    p.style.color = '#ffffff';
+    p.style.fontSize = 30 + 'px';
+
+    domELement.style.display = 'flex';
+    domELement.style.alignItems = 'center';
+    domELement.style.justifyContent = 'center';
+}
+
 // ! Анимация круга с помощью колбэка
